938f15488fa9d0368f5465353f43abc7625dc791
Parshuram2 <roosiinfidel09@gmail.com>
2021-09-23 14:00:31 +0530

    name

--- src/index.ts
+++ src/index.ts
@@ -101,7 +101,7 @@ app.get('/test', (req, res) => {
     const output = fib(req.query.code)
     console.log('finished');
 
-    res.status(200).json(output +  ' Hello from Staging');
+    res.status(200).json(output +  ' Hello from Staging first try');
 });
 
 app.get('/login', (req, res) => {
