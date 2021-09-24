5f03e7a7085aaf4a0a82f0a2f96eb45767ce9b6c
Parshuram2 <roosiinfidel09@gmail.com>
2021-09-24 09:46:46 +0530

    push change to master

--- src/index.ts
+++ src/index.ts
@@ -101,7 +101,7 @@ app.get('/test', (req, res) => {
     const output = fib(req.query.code)
     console.log('finished');
 
-    res.status(200).json(output +  ' Hello I have come from Staging!');
+    res.status(200).json(output +  ' Hello I have come from Staging Today!');
 });
 
 app.get('/login', (req, res) => {
